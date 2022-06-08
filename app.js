c22668a4d6cba6870e814a788a46ec61bf449496
James Alhassan <jamesalhassan052@gmail.com>
2022-06-08 16:17:11 +0100

    Add files via upload
    JS Date Setup

--- app.js
+++ app.js
@@ -21,3 +21,24 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format");
+
+let futureDate = new Date(2022, 11, 20, 23, 59, 59);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+
+const month = months[futureDate.getMonth()];
+
+const date = futureDate.getDate();
+
+const day = weekdays[futureDate.getDay()];
+
+const hours = futureDate.getHours();
+
+const minutes = futureDate.getMinutes();
+
+giveaway.textContent = `giveaway ends on ${day} ${date}th  ${month}, ${year} ${hours}:${minutes}pm`;
